27e095ca8ffc365ea13262a6f711529d077c3027
Felipe Norato <fnoratol@gmail.com>
2015-10-13 12:16:30 -0300

    Fix errors

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 var express = require('express')
   , load = require('express-load')
-  , routes = require('./routes');
-
-var app = express();
+  , app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
-
-app.get('/', routes.index);
-app.get('/usuarios', routes.user.index);
 
 load('models')
   .then('controllers')
   .then('routes')
   .into(app);
 
-app.listen(300, function () {
-  consol.log("Ntalk no ar");
+app.listen(3000, function () {
+  console.log("Ntalk no ar");
+});
--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,3 @@
-var express = require('express');
-var router = express.Router();
-
 module.exports = function (app) {
 	var home = app.controllers.home;
 	app.get('/', home.index); 
